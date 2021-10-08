73d46df922be72377487d3142207b12f4036f163
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-10-07 22:31:45 -0300

    feat(initial): criação do projeto bolão

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import styles from './styles.module.scss';
+
+const Login: NextPage = () => {
+  return <div className={styles.container}>foi</div>;
+};
+
+export default Login;
